21c6864293dec44adc77b315e365a08fcbe03117
Omar-gif-del <gabr3omar4366@gmail.com>
2025-10-25 20:06:44 +0300

    Fix base path for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/reda-sharm-explorer/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
